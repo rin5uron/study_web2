26cbd0d72992d1c473c1d1b264aed38ff2098c2d
r <rin5uron@gmail.com>
2025-07-28 23:15:24 +0900

    脱マウスと30個に向けて

--- practice/JavaScript/mentor-sessions/object/object30/.js
+++ practice/JavaScript/mentor-sessions/object/object30/.js
@@ -83,90 +83,103 @@ sweets1.printBuy('どこでも');
 
 
 
-// ③アイスクリームクラス：複数のアイスクリームを一括説明する
+// ③アイスクラス：複数のアイスクリームを一括説明する
 // インスタンス生成時に全プロパティを一気に初期化するパターン：
 class FrozenDesert {
     constructor(name,taste,howeat) {
         this.name = name;
+        this.type = type;
         this.taste = taste;
         this.howeat = howeat;
     }
 
 describe(){
-    console.log(this.name + 'は' + this.taste + 'です。' this.howeat + 'のがおすすめの食べ方です。' );
+    console.log(this.name + 'は' + this.type + 'です。'+ this.taste + 'で、' + this.howeat + 'のがおすすめの食べ方です。' );
 }
 }
 
-const icecream1 = new FrozenDesert
-('ハーゲンダッツマカダミアナッツ味','ごろごろマカダミアナッツが嬉しい美味しさ','好きな人と一緒に食べる');
-const icecream2 = new FrozenDesert
-('パリッテ','パリパリチョコが存分に楽しめるアイス','とにかくパリパリを楽しみたい時に食べる');
-const icecream3 = new FrozenDesert
-('パルム','絶妙な口溶けがなんとも言えない','可愛いフォルムをそっと愛でながらやさしい気持ちで味わう');
-const icecream3 = new icecream
-('ガリガリくんパイン味','リアルパイナップルを思わせる食感','パイナップルに想いを馳せて食べる');
+const ice1 = new FrozenDesert
+('パルム','アイスクリーム','絶妙な口溶けがなんとも言えないの','可愛いフォルムをそっと愛でながらやさしい気持ちで味わう');
+const ice2 = new FrozenDesert
+('パリッテ','アイスミルク','パリパリチョコが存分に楽しめるの','とにかくパリパリを楽しみたい時に食べる');
+const ice3 = new FrozenDesert
+('ブルーシールアイス','ラクトアイス','種類が多くて楽しいの','気分によってフレーバーを選ぶ');
+const ice4 = new FrozenDesert
+('ガリガリくん大人のパイン','氷菓','リアルパイナップルを思わせる食感','パイナップルに想いを馳せて食べる');
 
 // 配列にまとめて一括説明
-const menu = [ice1, ice2, ice3, ice4];
+const menu = [ice1, ice2, ice3, ];
 menu.forEach(item => item.describe());
 
 
-// // ④アイスクリームクラス
 
-// class icecreamBrand {
-//     constructor(brand) {
-//       this.brand = brand;
-//     }
+// ④アイスクリームクラス:ブランドごとにアイスクリームをまとめる
+// アイスクリームクラスの中にフレーバーオブジェクトを入れる
+
+// アイスクリームブランドクラス（親）
+class IcecreamBrand {
+    constructor(brand) {
+      this.brand = brand;
+      this.flavor = []; // フレーバーをしまっておく場所
+    }
+
+    // addFlavorメソッドを使用してflavorプロパティを登録できるようにする
+    addFlavor({ brand, name, taste, eatfeel }) {
+        const flavor = new Flavor({
+          brand,name,taste,eatfeel
+        });
+        this.flavors.push(flavor); // ブランドに登録！
+        return flavor; // 外でも使えるように返す
+      }
+    }
+
+// フレーバークラス（子）
+class Flavor {
+    constructor({ brand, name, taste, eatfeel }) {
+      this.brand = brand;
+      this.name = name;
+      this.taste = taste;
+      this.eatfeel = eatfeel;  
+    }
+
+  
+    describe() {
+      console.log(
+        `${this.brand}「${this.name}」味は${this.taste}ます。${eatfeel}気分の時に一緒に食べよ？`);
+    }
+}
+
+// （ブランドオブジェクト）
+const icecream1 = new icecreamBrand('ハーゲンダッツ');
+const icecream2 = new icecreamBrand('レディボーデン');
+const icecream3 = new icecreamBrand('ピノ');
+  
+// （インスタンスオブジェクト）
+
+  const macadamia = icecream1.addFlavor({
+    brand: 'ハーゲンダッツ',
+    name: 'マカダミアナッツ',
+    taste: '期待以上のマカダミアナッツ感があり',
+    eatfeel: '美味しいナッツが食べた'
+  });
+
+  const strawberry = icecream2.addFlavor({
+    brand: 'レディボーデン',
+    name: 'ストロベリー',
+    taste: '程よい甘酸っぱさがクセになり、いくらでも食べられ',
+    eatfeel: '思う存分満足するまでアイスクリームを食べたい'
+  });
+
+  const pinot = icecream3.addFlavor({
+    brand: 'ピノ',
+    name: 'アーモンド',
+    taste: '３種類の中で私の断トツお気に入りになり',
+    eatfeel: 'オレンジは私にちょうだい！あなたが赤と青が食べたい'
+  });
   
 
-//       const flavor = new Flavor({
-//         brand: this.brand,
-//         name: "",
-//         taste: "",
-//         point: "",
-//         place: ""
-//       });
-//       return flavor;
-//     }
-  
-//   class Flavor {
-//     constructor({ brand, name, taste, point, place }) {
-//       this.brand = brand;
-//       this.name = name;
-//       this.taste = taste;
-//       this.point = point;
-//       this.place = pl   ace;
-//     }
-  
-//     describe() {
-//       console.log(
-//         `${this.brand}「${this.name}」味は${this.taste}。ポイントは「${this.point}」。${this.place}で買うことができます。`
-//       );
-//     }
-//   }
-  
-//   // （ブランドオブジェクト）
-//   const haagenDazs = new icecreamBrand('ハーゲンダッツ');
-//   const Lady Borden = new icecreamBrand('レディボーデン');
-//   const palmu = new icecreamBrand('パルム');
-  
-//   // （インスタンス）
-//   const macadamia = haagenDazs.addFlavor({
-//     name: 'マカダミアナッツ',
-//     taste: '濃厚でコクがある',
-//     point: '期待以上のマカダミアナッツ感',
-//     place: 'どこでも'
-//   });
-  
-//   macadamia.describe();
-  
 
 
-
-
-// アイスクラス
-
-// アイスメーカーごとのクラス
 
 // // ④人間クラス職業
 // 人間クラス
